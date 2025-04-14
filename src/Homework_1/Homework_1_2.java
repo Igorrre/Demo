4fa283390cebceaf2a1699ef53e077cae9559b2e
Igor <igorrefx@gmail.com>
2025-04-14 18:36:09 +0300

    Added Homework_1

--- src/Homework_1/Homework_1_2.java
+++ src/Homework_1/Homework_1_2.java
@@ -3,9 +3,11 @@ package Homework_1;
 public class Homework_1_2 {
     public static void main(String[] args) {
 
-        int n = 71;
-        int a = (n % 10) + (n / 10);
+        int n = 75;
+        int a = n % 10;
+        int b = n / 10;
+        int sum = a + b;
 
-        System.out.println("Сумма двухзначного числа = " + a);
+        System.out.println("Сумма двухзначного числа = " + sum);
     }
 }
--- src/Homework_1/Homework_1_3.java
+++ src/Homework_1/Homework_1_3.java
@@ -3,9 +3,12 @@ package Homework_1;
 public class Homework_1_3 {
     public static void main(String[] args) {
 
-        int n = 171;
-        int a = (n % 10) + (n / 100) + (n % 100) / 10;
+        int n = 175;
+        int a = (n % 10);
+        int b = (n / 100);
+        int c = (n % 100) / 10;
+        int sum = a + b + c;
 
-        System.out.println("Сумма трехзначного числа = " + a);
+        System.out.println("Сумма трехзначного числа = " + sum);
     }
 }
